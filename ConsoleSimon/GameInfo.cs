1745c186733dc50bc00521b0c7475149c8a3cc5a
John Olson <john.olson@waystar.com>
2025-06-09 11:02:41 -0600

    Starter code up. Gives random number (but too long) and correctly fails you when you get it wrong

---
+++ ConsoleSimon/GameInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSimon
+{
+    public class GameInfo
+    {
+        public List<int> NumSequence { get; set; } = [];
+
+        public void AddNewRandomInt()
+        {
+            Random random = new Random();
+            this.NumSequence.Add(random.Next());
+        }
+    }
+}
---
+++ ConsoleSimon/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleSimon;
+Console.Clear();
+Console.WriteLine("Let's play Simon!\nPress any key and enter to begin\n");
+Console.ReadLine();
+Console.Clear();
+bool lost = false;
+GameInfo gameInfo = new GameInfo();
+while (!lost)
+{
+    gameInfo.AddNewRandomInt();
+    for (int i = 0; i < gameInfo.NumSequence.Count; i++)
+    {
+        Console.WriteLine(gameInfo.NumSequence[i]);
+        Thread.Sleep(1000);
+        Console.Clear();
+        string userResponse = Console.ReadLine();
+        int intResponse = Convert.ToInt32(userResponse);
+        if (intResponse != gameInfo.NumSequence[i])
+        {
+            Console.WriteLine($"Wrong answer! Correct answer is {gameInfo.NumSequence[i]}. " +
+                $"Game over. You scored {gameInfo.NumSequence.Count - 1}. Press any key to end.\n");
+            Console.ReadLine();
+            lost = true;
+            break;
+        }
+    }
+}
+
