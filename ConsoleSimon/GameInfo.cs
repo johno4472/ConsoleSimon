b2aea8c9b5ec17418ecf1d2eb97d0d68957f018f
John Olson <john.olson@waystar.com>
2025-06-16 14:55:48 -0600

    Updated architecture to method decomposition

--- ConsoleSimon/GameInfo.cs
+++ ConsoleSimon/GameInfo.cs
@@ -10,10 +10,68 @@ namespace ConsoleSimon
     {
         public List<int> NumSequence { get; set; } = [];
 
-        public void AddNewRandomInt()
+        public bool GameOver = false;
+
+        public int CorrectAnswer {  get; set; }
+
+        public int Score { get; set; }
+
+        public GameInfo AddNewNumber()
         {
-            Random random = new Random();
+            Random random = new();
             this.NumSequence.Add(random.Next(10));
+            return this;
+        }
+
+        public GameInfo IntroduceLevel()
+        {
+            if (NumSequence.Count > 0)
+            {
+                Console.WriteLine("Next level!\n");
+                Thread.Sleep(500);
+                Console.Clear();
+            }
+            else { Console.WriteLine("First Level!"); }
+            return this;
+        }
+
+        public GameInfo PrintNumberSequence()
+        {
+            for (int i = 0; i < NumSequence.Count; i++)
+            {
+                Console.Write(NumSequence[i]);
+                Thread.Sleep(1000);
+                Console.Clear();
+                Thread.Sleep(500);
+            }
+            return this;
+        }
+
+        public GameInfo TestNumberSequence()
+        {
+            for (int i = 0; i < NumSequence.Count; i++)
+            {
+                Console.Write($"Enter number {i + 1}: ");
+                string userResponse = Console.ReadLine() ?? "";
+                try
+                {
+                    int intResponse = Convert.ToInt32(userResponse);
+
+                    if (intResponse != NumSequence[i])
+                    {
+                        CorrectAnswer = NumSequence[i];
+                        Score = NumSequence.Count - 1;
+                        GameOver = true;
+                        break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("You didn't even enter a valid number. try again");
+                    i--;
+                }
+            }
+            return this;
         }
     }
 }
--- ConsoleSimon/Program.cs
+++ ConsoleSimon/Program.cs
@@ -1,40 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleSimon;
+
 Console.Clear();
 Console.WriteLine("Let's play Simon!\nPress any key and enter to begin\n");
 Console.ReadLine();
 Console.Clear();
-bool lost = false;
-GameInfo gameInfo = new GameInfo();
-while (!lost)
+
+GameInfo gameInfo = new();
+
+while (!gameInfo.GameOver)
 {
-    if (gameInfo.NumSequence.Count > 0)
-    {
-        Console.WriteLine("Next level!\n");
-        Thread.Sleep(500);
-        Console.Clear();
-    }
-    gameInfo.AddNewRandomInt();
-    for (int i = 0; i < gameInfo.NumSequence.Count; i++)
-    {
-        Console.Write(gameInfo.NumSequence[i]);
-        Thread.Sleep(1000);
-        Console.Clear();
-        Thread.Sleep(500);
-    }
-    for (int i = 0; i < gameInfo.NumSequence.Count; i++)
-    {
-        Console.Write($"Enter number {i + 1}: ");
-        string userResponse = Console.ReadLine();
-        int intResponse = Convert.ToInt32(userResponse);
-        if (intResponse != gameInfo.NumSequence[i])
-        {
-            Console.WriteLine($"Wrong answer! Correct answer is {gameInfo.NumSequence[i]}. " +
-                $"Game over. You scored {gameInfo.NumSequence.Count - 1}. Press any key to end.\n");
-            Console.ReadLine();
-            lost = true;
-            break;
-        }
-    }
+    gameInfo.IntroduceLevel()
+
+        //Adds a new number to the end of the list
+        .AddNewNumber()
+
+        //Prints all numbers in the sequence one at a time
+        .PrintNumberSequence()
+
+        //Tests plaMarks as game over when number incorrectly reported
+        .TestNumberSequence();    
 }
 
+Console.WriteLine($"Wrong answer! Correct answer is {gameInfo.CorrectAnswer}. " +
+                        $"Game over. You scored {gameInfo.Score}. Press any key to end.\n");
+Console.ReadLine();
